7015794063795b6f2c427f8753594409748016eb
LTPhat <lamthanhphat0918398911@gmail.com>
2023-10-02 20:43:56 +0700

    complete ML baseline

--- src/models/baseline.py
+++ src/models/baseline.py
@@ -8,7 +8,7 @@ import sys
 import os
 parent_root = os.path.abspath(os.path.join(os.path.dirname(os.path.abspath(__file__)), os.pardir))
 sys.path.append(parent_root)
-from src.dataset import Dataset
+from dataset import *
 
 from config.configs import Params
 param = Params()
@@ -35,4 +35,26 @@ class BaseLine(object):
         print("Model accuracy on test set", model.score(X_test, y_test))
         return val_score, test_score
     
+if __name__ == "__main__":
+    print("---------------Baseline ML model------------------------")
+    params = Params()
+    # Define dataset
+    dataset = Dataset(train_txt=params.TRAIN_DIR, val_txt=params.VAL_DIR, test_txt=params.TEST_DIR, num_inputs=1)
+
+    # Define train/val/test
+    train_sentences = dataset.train_sentences
+    val_sentences = dataset.val_sentences
+    test_sentences = dataset.test_sentences
+    y_train = dataset.y_train
+    y_val = dataset.y_val
+    y_test = dataset.y_test
+
+    val_score, test_score =BaseLine(vectorizer=TfidfVectorizer(), 
+                                    model=LogisticRegression()).train(
+                                    X_train=train_sentences, y_train=y_train,
+                                    X_val=val_sentences, y_val=y_val,
+                                    X_test=test_sentences, y_test=y_test)
     
+
+    print("Baseline Val score: ", val_score)
+    print("Baseline Test score: ", test_score)
