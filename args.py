f8db9bcfb70686c8e1f074d46d466fd37cb0d9a7
LTPhat <lamthanhphat0918398911@gmail.com>
2023-10-02 22:34:55 +0700

    arguments CLI

---
+++ args.py
@@ -0,0 +1,51 @@
+import argparse
+
+
+valid_models = ["hybrid", "att", "tf_decoder", "penta"]
+valid_embeddings = [str(None), "glove", "bert"]
+
+
+
+def init_argparse():
+    parser = argparse.ArgumentParser(
+    prog="Training Model",
+    usage="Arguments: --model, --embedding, --batch_size(optional).",
+    description="""Example: python main.py --model penta --embedding None --dataset_size 1 --batch_size 32 -- epochs 3
+                 --model: Type of model for training. Expect one of ['hybrid', 'att', 'tf_decoder', 'penta'].
+                 --embedding: Type of word-level embedding. Expect one of [None, 'glove', 'bert'].
+                 --dataset_size: Dataset size for training. Default: 1 (All dataset).
+                 --batch_size: Batch size. Default: 32.
+                 --epochs: Epochs. Default: 3."""
+)
+    parser.add_argument("--model", required=True, help='Type of model: hybrid, att, tf_decoder, penta')
+    parser.add_argument(
+        "--embedding", required=True,
+        help='Word embedding: None, Glove or BERT'
+    )
+    parser.add_argument(
+        "--dataset_size", required=False, default= 1, help= "Dataset size"
+    )
+    parser.add_argument(
+        "--batch_size",required=False ,default = 32,
+        help='Batch size'
+    )
+    parser.add_argument(
+        "--epochs",required=False ,default = 3,
+        help='Epochs'
+    )
+    return parser
+
+
+def check_valid_args(args):
+    # Check valid model type
+    if str(args.model).lower() not in valid_models:
+        raise TypeError("No model named: {}, expected valid model belongs to {}".format(args.model, valid_models))
+    elif str(args.embedding) not in valid_embeddings:
+        raise TypeError("No embedding type named: {}, expeted valid embedding belongs to {}".format(args.embedding, valid_embeddings))
+    
+    return True
+
+
+if __name__ == "__main__":
+    parser = init_argparse()
+    args   = parser.parse_args()
